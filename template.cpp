94e818a2d28679e86826958a882e6613118be536
lyj00912 <3122742760@qq.com>
2021-02-13 18:04:55 +0800

    modified vs.cpp to make it work better

--- template.cpp
+++ template.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 template <class T>
 
-void swap(T& a, T& b) {
+void swap(T a, T b) {
 	T temp = a;
 	a = b;
 	b = temp;
--- vs.cpp
+++ vs.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
 #include <Windows.h>
 #include <string.h>
+#include <io.h>
 using namespace std;
 
+inline bool checkIfFileExist(const char* name) {
+	return (access(name, 0) != -1);
+}
+
+void runFileWithVS(const char* filename) {
+	char buffer[100] = "devenv ";
+	strcat(buffer, filename);
+	//cout << "Running Command: " << buffer << endl;
+	system(buffer);
+}
+
+void createNewFile(const char* filename) {
+	char buffer[100] = "type nul > ";
+	strcat(buffer, filename);
+	//cout << "Running Command: " << buffer << endl;
+	system(buffer);
+}
+
+
+
 int main(int argc, char* argv[]) {
+	char* filename;
+
 	if (argc != 2) {
 		cout << "INVALID ARGUMENTS, TERMINATED." << endl;
+		return 0;
 	}
 	else {
-		char buffer[100] = "devenv ";
-		strcat(buffer, argv[1]);
-		cout <<"Running Command: "<< buffer << endl;
-		system(buffer);
+		filename = argv[1];
 	}
+
+	if (checkIfFileExist(filename) == true) {
+		cout << "Loading.." << endl;
+		runFileWithVS(filename);
+	}
+	else {
+		cout << "File Not Exist." << endl;
+
+		cout << "Creating File..." << endl;
+		createNewFile(filename);
+
+		cout << "Loading.." << endl;
+		runFileWithVS(filename);
+	}
+	
 }
