da16a0601c3dca5f20bfb14b18396185567fd2cf
lyj00912 <3122742760@qq.com>
2021-07-09 19:14:07 +0800

    created my first derived class in inheritance.cpp

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
-using namespace std;
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+typedef int i32;
+typedef float f32;
+
+
+class Person{
+public:
+    string firstName;
+    string lastName;
+    i32 age;
+    Person(string fn, string ln, i32 ag) : firstName(fn), lastName(ln), age(ag) {}
+    void ShowInfo(){
+        cout << firstName << " " << lastName << ", " << age << " years old." << endl;
+    }
+};
+
+
+class Student : public Person{
+public:
+    i32 grade;
+    Student(string fn, string ln, i32 ag, i32 gd) : Person(fn, ln, ag), grade(gd) {}
+    void ShowGrade(){
+        cout << "Grade " << grade << endl;
+    }
+};
+
+
 int main(){
-    cout<<"hlwd"<<endl;
+    Person ps("Elon", "Musk", 50);
+    ps.ShowInfo();
+
+    Student st("Jackie", "Lin", 16, 9);
+    st.ShowInfo();
+    st.ShowGrade();
 }
